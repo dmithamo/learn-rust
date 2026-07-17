0d50c1673c47492f5ed2060495a5da38cab7f36f
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-07-17 09:38:36 +0300

    feat: implement actual random access, overcome compiler optimisations in release, and learn a tonne while at it

--- src/ebp-ecs-book/ex-machine-model/src/main.rs
+++ src/ebp-ecs-book/ex-machine-model/src/main.rs
@@ -1,31 +1,33 @@
-use std::time::{self, Duration};
+use rand::{self, seq::SliceRandom};
+use std::time::{self};
 use thousands::Separable;
 
 fn main() {
     let counts: Vec<usize> = vec![
+        100,
         1_000,
         10_000,
         100_000,
         1_000_000,
         10_000_000,
         100_000_000,
-        1_000_000_000,
+        // 1_000_000_000,
     ];
-    let mut durations: Vec<(usize, Duration, Duration)> = vec![];
+    let mut durations: Vec<(usize, f64, f64)> = vec![];
     for c in counts {
         durations.push((c, sequential_access_time(c), random_access_timing(c)));
     }
 
-    let print_width = 20;
+    let print_width = 24;
     println!("Machine model exercise: Duration for array access");
     println!(
         "{:<print_width$}{:>print_width$}{:>print_width$}",
-        "Count", "Sequential", "Random",
+        "Count", "Sequential(ns)", "Random(ns)",
     );
     println!("{}", "_".repeat(print_width * 3));
     for d in durations {
         println!(
-            "{:<print_width$}{:>print_width$?}{:>print_width$?}",
+            "{:<print_width$}{:>print_width$.2?}{:>print_width$.2?}",
             (d.0).separate_with_commas(),
             d.1,
             d.2
@@ -33,23 +35,28 @@ fn main() {
     }
 }
 
-fn sequential_access_time(count: usize) -> Duration {
+fn sequential_access_time(count: usize) -> f64 {
     let a = vec![1 as u64; count];
 
     let start = time::Instant::now();
-    let _ = a.iter().sum::<u64>();
+    std::hint::black_box(a.iter().sum::<u64>());
 
-    (time::Instant::now() - start) / count as u32
+    (time::Instant::now() - start).as_nanos() as f64 / count as f64
 }
 
-fn random_access_timing(count: usize) -> Duration {
+fn random_access_timing(count: usize) -> f64 {
     let a = vec![1 as u64; count];
     let mut _sum = 0u64;
 
+    let mut indices: Vec<usize> = (0..count).collect();
+    let mut rng = rand::rng();
+    indices.shuffle(&mut rng);
+
     let start = time::Instant::now();
-    for j in 0..count {
+    for &j in &indices {
         _sum += a[j];
     }
 
-    (time::Instant::now() - start) / count as u32
+    std::hint::black_box(_sum);
+    (time::Instant::now() - start).as_nanos() as f64 / count as f64
 }
